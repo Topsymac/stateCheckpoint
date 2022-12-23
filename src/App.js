73d9a43e62102132c2f3dc3e76e5fe4f833b568d
Topsymac <temitopsy35@gmail.com>
2022-12-23 06:19:51 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from "react";
+// import picture from "../src/Images/cute-black-girl-afro-painting-lvk32tfg8zuweknt.jpg"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    person: {
+      fullname: "Macaulay Temitope",
+      bio: "I am training to be a junior frontend developer",
+      imgSrc: "https://wallpapers.com/images/high/cute-black-girl-afro-painting-lvk32tfg8zuweknt.webp",
+      profession: "Frontend developer",
+    },
+    show: true,
+  };
+
+  handleToggle = () => {
+    this.setState({ show: !this.state.show });
+  };
+
+  render() {
+    return (
+      <div style={{width:"50%",display: "grid", alignItems: "center", margin: "0px auto"}}>
+        <div className={this.state.show ? "show" : "notShow"}>
+          <img src={this.state.person.imgSrc} style={{width: "200px", height:"400px", borderRadius:"60%"}} alt="profile" />
+          <h1>{this.state.person.fullname}</h1>
+          <h5>{this.state.person.profession}</h5>
+          <p>{this.state.person.bio}</p>
+        </div>
+        <button onClick={this.handleToggle} style={{backgroundColor: "darkcyan", padding: "20px",border: "solid", borderRadius: "30px", fontSize: "15px", marginTop: "20px"}}>Toggle Profile</button>
+      </div>
+    );
+  }
 }
 
 export default App;
